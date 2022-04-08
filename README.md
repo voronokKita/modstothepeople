25ffc553286fa53889db2c50a5bf5d5165f65250
Kita <kitavoronok@gmail.com>
2022-04-08 22:14:31 +0300

    v3

--- README.md
+++ README.md
@@ -7,32 +7,34 @@ In those cases when Steam Workshop isn't accessible, or you don't want to use it
 
 ### Here is how it works
 
-0. unpack your mods, give to the folders some simple names
-1. put the mods and the script into '/Paradox Interactive/your game folder/mod/'
-2. run a terminal command 'python3 mttp.py'
-3. or 'python3 mttp.py [your game version]'
+0. unpack your mods, (give to the folders some good names if you want)
+1. put the mods and the script into '/Documents/Paradox Interactive/your game folder/mod/'
+2. run in this filder a terminal command 'python3 mttp.py'
+3. or 'python3 mttp.py [your game version] to overwrite it'
 
-The script can be used for a cleanup, if you change your set of mods then it removes old files & records.
+If you change your set of mods then it'll cleanup old files & records.
 
 ## Detailed
 
 You'll need Python 3 installed. It comes with Ubuntu by default and can be included in other OS as well. Check in the terminal:
+
 > python3 --version
 
 You can get it here https://www.python.org/downloads/<br>
 If you stuck then search "install python 3 in [your OS]", there are many detailed guides out there.
 
 The script will definitely work on EU4 and HOI4, and probably on other games from this list:
+
 > https://en.wikipedia.org/wiki/Paradox_Development_Studio#List_of_games_developed
 
 You install mods in the 'Paradox Interactive' folder, that will be created after the first lunch of a game. On Windows and Mac it will be created in your 'Documents' folder, on Linux in '~/.local/share/' of your home catalog.<br>
-Normal order of mod installation in Europa Universalis IV by hands is:
+Normal order of mod installation on Europa Universalis IV by hands is:
 
 0. Copy descriptor.mod, that normally found inside a mod folder, into the<br>
 /Paradox Interactive/Europa Universalis IV/mod/<br>
 1. in the end of this descriptor file add a new line<br>
 path="path to the mod's folder"<br>
-2. Next you enable the mod by making/editing this file<br>
+2. Next you enable the mod by making/editing file<br>
 /Paradox Interactive/Europa Universalis IV/dlc_load.json<br>
 format of the file is this<br>
 {"disabled_dlcs" :[], "enabled_mods": ["mod/mod name 1.mod", "mod/mod name 2.mod", "mod/mod name 3.mod"]}
--- mttp.py
+++ mttp.py
@@ -1,38 +1,66 @@
 #! python3
 """ (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-v1 winter 2022
-v2 spring 2022
-TODO remote id"""
+v1 january 2022
+v2 march 2022
+v3 april 2022"""
 import re
 import sys
 import json
-import random
 import pathlib
-import collections
+
 
 PATTERN = re.compile(r'\d+\.\d+(\.\d+)?')
 MAIN_DIR = pathlib.Path.cwd()
 DLC_LOAD = MAIN_DIR.parent / "dlc_load.json"
-GAME_VERSION = "*.*.*"
+GAME_VERSION = ""
+
+
+class Mod:
+    def __init__(self, name, folder, descriptor, data=None):
+        self.name = name
+        self.folder = folder
+        self.descriptor = descriptor
+        self.data = data
+
+    def save(self, target_dir):
+        new_descriptor = target_dir / f"{self.name}.mod"
+        with open(new_descriptor, 'w') as file:
+            file.write(self.data)
+        print(f"saved {self}")
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 def main():
-    if len(sys.argv) > 1:
+    if MAIN_DIR.parent.parent.stem != "Paradox Interactive":
+        print("Run the script in the /Documents/Paradox Interactive/your game folder/mod/",
+              f"Current working directory is {MAIN_DIR}",
+              "Continue anyway? [y/n]:", sep="\n", end=" ")
+        answer = input().lower().strip()
+        if not answer in ["y", "yes"]:
+            sys.exit(1)
+
+    elif len(sys.argv) > 1:
         if not PATTERN.fullmatch(sys.argv[1]):
             print(f"Enter a game version in a format like 'python3 {sys.argv[0]} 3.2.1'")
-            sys.exit(1)
+            sys.exit(2)
         else:
             global GAME_VERSION
-            GAME_VERSION = sys.argv[1]
+            GAME_VERSION = f'supported_version="{sys.argv[1]}"'
 
     try:
-        mods_dirs = paths_handler()
-        mods_data, json_data = processing(mods_dirs)
-        writer(mods_data, json_data, mods_dirs)
-
+        mods = paths_handler()
+        mods, json_data = processing(mods)
+        writer(mods, json_data)
     except OSError:
         print("Error read/write data. Don't have permissions?")
-        sys.exit(2)
+        sys.exit(3)
+    except Exception as error:
+        print("Unknown error occurred. Details:",
+              "-" * 10, f"{error}", "-" * 10,
+              "You may contact with me and I'll try to fix it!", sep="\n")
+        sys.exit(4)
     else:
         print("Done.")
         sys.exit(0)
@@ -40,58 +68,58 @@ def main():
 
 def paths_handler():
     print("directory:", MAIN_DIR)
-
     mods_dirs = [item for item in MAIN_DIR.iterdir() if item.is_dir()]
-    if len(mods_dirs) == 0:
-        print("No mods found.")
+
+    mods = []
+    for directory in mods_dirs:
+        descriptor = [item for item in directory.iterdir() if item.name.endswith( ('.mod', '.mod.old') )]
+        if not descriptor:
+            print(f"mod {directory.stem} doesn't have a descriptor file; skipped")
+            continue
+        else:
+            descriptor = descriptor[0]
+        mod = Mod(directory.stem, directory, descriptor)
+        mods.append(mod)
+
+    if not mods:
+        print("no mods found.")
         sys.exit(0)
     else:
-        print(len(mods_dirs), "mods found:")
-        print(*[f"{item.stem}" for item in mods_dirs], sep=', ')
+        print(len(mods), "mods found")
 
-    return mods_dirs
+    return mods
 
 
-def processing(mods_dirs):
-    mods_data = {}
+def processing(mods):
     enabled_mods = []
-    supported_version = f'supported_version="{GAME_VERSION}"'
-    for mod_path in mods_dirs:
-        name = f'name="{mod_path.stem}"'
-        path = f'path="{mod_path}"'
-        thumbnail = get_picture(mod_path)
+    data = ""
+    for mod in mods:
+        with open(mod.descriptor, 'r', encoding='utf-8') as file:
+            for line in file:
+                if GAME_VERSION and "supported_version" in line:
+                    line = GAME_VERSION + "\n"
+                data += line
+        data += f'\npath="{mod.folder}"\n'
+        mod.data = data
 
-        mods_data[mod_path] = f"{name}\n{supported_version}\n{thumbnail}\n{path}\n"
-
-        enabled_mods.append(f"mod/{mod_path.stem}.mod")
+        enabled_mods.append(f"mod/{mod.name}.mod")
 
     json_data = {'disabled_dlcs': [], 'enabled_mods': enabled_mods}
-    return mods_data, json_data
+    return mods, json_data
 
 
-def get_picture(mod_path):
-    thumbnail = ""
-    for item in mod_path.iterdir():
-        if item.is_file() and "thumbnail" in item.name:
-            thumbnail = item.name
-    if not thumbnail:
-        pics = [item.name for item in mod_path.iterdir() if item.name.endswith( ('.jpg', '.jpeg', '.png') )]
-        thumbnail = random.choice(pics) if len(pics) > 0 else ""
-    return f'picture="{thumbnail}"'
-
-
-def writer(mods_data, json_data, mods_dirs):
+def writer(mods, json_data):
     [item.unlink() for item in MAIN_DIR.iterdir() if item.name.endswith('.mod')]
-    for mod in mods_dirs:
-        [resource.rename(f"{resource}.old") for resource in mod.iterdir() if resource.name.endswith('.mod')]
 
-    for mod in mods_data:
-        descriptor_file = MAIN_DIR / f"{mod.stem}.mod"
-        with open(descriptor_file, 'w') as f:
-            f.write(mods_data[mod])
+    for mod in mods:
+        if mod.descriptor.name.endswith('.mod'):
+            mod.descriptor.rename(f"{mod.descriptor}.old")
 
-    with open(DLC_LOAD, 'w') as f:
-        json.dump(json_data, f)
+    for mod in mods:
+        mod.save(MAIN_DIR)
+
+    with open(DLC_LOAD, 'w') as file:
+        json.dump(json_data, file)
 
 
 if __name__ == '__main__':
